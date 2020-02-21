8e37a1ed228aff335a5001812a7d716005ec3dcd
Gautier <laisne.gautier@gmail.com>
2020-02-21 12:31:50 +0100

    Menu principal debut

--- start.pl
+++ start.pl
@@ -1,3 +1,24 @@
 :- consult('players.pl').
 :- consult('board.pl').
 :- consult('game.pl').
+
+menu :- repeat,
+    write('Combien de joueurs ? (q pour quitter)'),nl,
+    write('    -> '), read(Choix),
+    (
+        integer(Choix), Choix > 0, Choix < 5 -> creationJeu(Choix), !;
+        Choix == 'q' -> !
+    ).
+
+creationJeu(N):- 
+    write('Vous avez choisi '), write(N), write(' joueurs'), nl.
+    a_FaireJoueurs(N), 
+    repeat,   
+    % TO DO
+    % Gerer le joueur en cours avec un assert
+
+
+
+:-menu.
+
+
