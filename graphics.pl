b33078c789bf658728aa6a3764186c11aad56f05
Grenier Célestin <cgrenier003@ensc.fr>
2020-03-11 12:02:10 +0100

    Commentaires

--- graphics.pl
+++ graphics.pl
@@ -29,23 +29,25 @@ displCaseExist(Pos, L, EstSniper) :-
 displCaseVide((X,Y), L) :-
     X2 is X-1, Y2 is Y-1,
     (
-        L == 0, (
-            case((X,Y2), _),
+        L == 0, ( % if (ligne 0 
+            case((X,Y2), _), % ET case au dessu exist)
             write('+----') ,!
-        ) ; (
-            case((X2,Y), _),
+        ) ; ( % else
+            case((X2,Y), _), % if (case a gauche existe)
                 ( L == 0, write('+') ,!; write('|') ) ,!
-            ;
+            ; % else (le + c'est pour les angles bas-droit des extemitées)
                 L == 0,case((X2,Y2), _), write('+') ,!; write('.')
         ), write('....')
     ).
 
+% affiche une case si elle existe, sinon une case vide
 displCase(Pos, L) :-
     case(Pos, EstSniper),   % if (il existe une case a Pos) {
         displCaseExist(Pos, L, EstSniper) ,!
     ;                       % } else
         displCaseVide(Pos, L).
 
+% affiche les numéros sur le côté gauche
 displNumLigne(J, L) :-
     ( L == 1,
         format('~|~` t~d~3+', [ J ]), write('  ') ,!
@@ -53,29 +55,34 @@ displNumLigne(J, L) :-
         write('     ')
     ).
 
+% affiche les numéros en haut
 displNumColonnes(N) :-
     write('     '),
     between(0, N, I),
         format('~|~` t~d~3+', [ I ]), write('  '),
     fail.
 
-trouveTailleTerrain(MaxX, MaxY) :-
+trouveTailleTerrain(TaillX, TaillY) :-
     findall(X, case((X,_), _), ListX),
     findall(Y, case((_,Y), _), ListY),
-    max_list(ListX, MaxX),
-    max_list(ListY, MaxY).
+    max_list(ListX, TaillX),
+    max_list(ListY, TaillY).
 
 % verticalement : permière coordonnée (numéro colonne)
-d_Terrain() :-
+displTerrain() :-
     trouveTailleTerrain(MaxX, MaxY),
+
+    % les limites sont +1 pour afficher le contours bas et droite des cases au bord bas droit du terrain
     LimX is MaxX+1, LimY is MaxY+1,
     write(MaxX), nl, write(MaxY), nl,
 
     \+ displNumColonnes(MaxX), nl,
 
     between(0, LimY, J),
+        % scanlines (chaque case fait 3 lignes)
         between(0, 2, L),
             nl,
+            % comme on va jusqu'a taille+1, on skip dès qu'on a atteint la limite
             ( J == LimY, write('     ') ,!; displNumLigne(J, L) ),
             between(0, LimX, I),
                 displCase((I, J), L),
@@ -83,7 +90,8 @@ d_Terrain() :-
         fail,
     fail.
 
-da() :- d_Terrain().
+d_Terrain() :-
+    displTerrain().
 
 d_Detail(Pos) :-
     case(Pos, _),
