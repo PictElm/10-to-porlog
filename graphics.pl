b3a69b28977102e5e1f07e50927c5c8bf860309a
Grenier Célestin <cgrenier003@ensc.fr>
2020-03-11 10:03:52 +0100

    Affichaghe des cases complètes
                      +----+
                      | Px |
                      |    |
                      +----+
          +----++----++----+      +----+
          | P  || Px || P  |      | P  |
          |    ||    ||    |      |    |
          +----++----++----+      +----+
    +----++----+      +----++----++----+
    | Px || P  |      | P  || P  || Px |
    |    ||    |      |    ||    ||    |
    +----++----+      +----++----++----+
          +----++----++----++----+
          | Px || P  || Px || Px |
          |    ||    ||    ||    |
          +----++----++----++----+
                +----++----+
                | Px || P  |
                |    ||    |
                +----++----+

--- graphics.pl
+++ graphics.pl
@@ -4,30 +4,38 @@
 % +----+
 
 displCase(Pos, L) :-
-    case(Pos, EstSniper),
-    ( L == 0,               % if (L == 0) première ligne
+    case(Pos, EstSniper),   % if (il existe une case a Pos) {
+    ( L == 0,               %   if (L == 0) première ligne
         write('+----+') ,!
-    ; L == 1,               % if (L == 1) ligne ?policier et ?sniper
-        write('|  '),
+    ; L == 1,               %   if (L == 1) ligne ?policier et ?sniper
+        write('| '),
+        ( personnage(Perso, _, vivant), policier(Perso),
+            write('P') ,!
+        ;
+            write(' ')
+        ),
         ( EstSniper == true,
             write('x') ,!
         ;
             write(' ')
         ),
         write(' |') ,!
-    ; L == 2,               % if (L == 2) ligne nombre de perso
+    ; L == 2,               %   if (L == 2) ligne nombre de perso
         write('|    |') ,!
     ;
         write('+----+')
-    ).
-
+    ) ,!
+    ;                       % } else {
+        write('      ').
 
 test() :-
-    between(0, 1, I),
+    between(0, 5, I),
         between(0, 3, L),
-            displCase((I, 3), L)
+            nl,
+            between(0, 5, J),
+                displCase((I, J), L),
+            fail,
         fail,
-        nl,
     fail.
 
 % terrLigne(N) :-
